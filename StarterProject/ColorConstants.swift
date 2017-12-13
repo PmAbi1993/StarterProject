25dcb82436ec2c21a165ab5e82c587da788ce472
PmAbi1993 <ylikeme4ever@gmail.com>
2017-12-13 10:39:06 +0530

    Added Tabs and Sliders

---
+++ StarterProject/ColorConstants.swift
@@ -0,0 +1,33 @@
+//
+//  ColorConstants.swift
+//  StarterProject
+//
+//  Created by Binshad on 29/11/17.
+//  Copyright © 2017 PmABi. All rights reserved.
+//
+
+import Foundation
+
+
+class ColorConstants{
+
+    
+    
+
+    /**
+     An example of using a number sign (`#`) for a *heading*
+     
+     ## This is a Heading 2
+     */
+    static let WetAsphalt = "#34495e"
+
+    /**
+     WetAsphalt
+
+       ![Xcode icon](http://devimages.apple.com.edgekey.net/assets/elements/icons/128x128/xcode.png)
+     
+     */
+    static let WetAsphalt2 = "#34495e"
+
+    
+}
---
+++ StarterProject/UIView.swift
@@ -0,0 +1,31 @@
+//
+//  UIView.swift
+//  StarterProject
+//
+//  Created by Binshad on 29/11/17.
+//  Copyright © 2017 PmABi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIView{
+
+    
+    
+    func makeViewAsDefaultcard(){
+    
+        layer.cornerRadius = 3
+        layer.shadowColor = UIColor(red: 0/255.5, green: 0/255.5, blue: 0/255.5, alpha: 1.0).cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 1.75)
+        layer.shadowRadius = 1.7
+        layer.shadowOpacity = 0.45
+        
+    }
+
+    
+    
+
+
+}
